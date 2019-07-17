749269524e363cc980a6d644dee650a92e3cbf7f
Dan Tao <daniel.tao@gmail.com>
2019-07-17 12:59:33 -0500

    Add basic documentation to modlist module

--- modlist/__init__.py
+++ modlist/__init__.py
@@ -1,21 +1,54 @@
-"""Library for declarative list manipulation."""
+"""
+Library for declarative list manipulation.
+
+This library consists of a single method, `modlist`, which accepts a list and
+a small number of keywords arguments describing modifications to apply to the
+list. These modifications are non-destructive and are performed on a copy of
+the list, which is then returned.
+"""
 
 
-def modlist(lst, **kwargs):
-    """Perform a series of operations on a list."""
+def modlist(lst, prepend=None, append=None, insert=None, remove=None,
+            move=None):
+    """Perform a series of operations on a list.
+
+    :param prepend:
+        An optional list of values to prepend to the beginning of the list.
+
+    :param append:
+        An optional list of values to append to the end of the list.
+
+    :param insert:
+        An optional list of tuples describing insertions to be made into the
+        list. Each tuple should have the form (value, location) where location
+        is a dict comprising the keys 'before' and/or 'after' and specifying
+        values before/after which the given value should be inserted.
+
+    :param remove:
+        An optional list of values to remove from the list. Note that if a
+        value appears multiple times in the list, it will only be removed once
+        per specified removal.
+
+    :param move:
+        An optional list of tuples indicating values that should be moved to a
+        new location in the list. Each tuple should have the form
+        (value, location) where location is a dict comprising the keys 'before'
+        and/or 'after' and specifying values before/after which the given value
+        should be moved.
+    """
     output = list(lst)
 
-    if 'prepend' in kwargs:
-        output = kwargs['prepend'] + output
+    if prepend:
+        output = prepend + output
 
-    if 'append' in kwargs:
-        output = output + kwargs['append']
+    if append:
+        output = output + append
 
-    def remove(value):
+    def remove_item(value):
         index = output.index(value)
         output.pop(index)
 
-    def insert(value, location):
+    def insert_item(value, location):
         if location.get('after'):
             after_index = output.index(location['after'])
             output.insert(after_index + 1, value)
@@ -23,15 +56,18 @@ def modlist(lst, **kwargs):
             before_index = output.index(location['before'])
             output.insert(before_index, value)
 
-    for value, location in kwargs.get('insert', []):
-        insert(value, location)
+    if insert:
+        for value, location in insert:
+            insert_item(value, location)
 
-    for removal in kwargs.get('remove', []):
-        remove(removal)
+    if remove:
+        for removal in remove:
+            remove_item(removal)
 
-    for value, location in kwargs.get('move', []):
-        remove(value)
-        insert(value, location)
+    if move:
+        for value, location in move:
+            remove_item(value)
+            insert_item(value, location)
 
     if isinstance(lst, tuple):
         output = tuple(output)
